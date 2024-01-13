492d6fccafadea8bfb09ac646c0f23595266a724
bdunn20 <bdunn20@gmail.com>
2024-01-12 20:36:20 -0600

    Ch5_Pt5_Chaining Complete

---
+++ Python_Interactive_Dashboards_with_Plotly_Dash/Course/Ch5_Pt5_Chaining.py
@@ -0,0 +1,61 @@
+from dash import Dash, html, dcc, Input, Output
+import pandas as pd
+import plotly.express as px
+
+happiness = pd.read_csv('world_happiness.csv')
+
+app=Dash()
+
+app.layout=html.Div([
+    html.H1('World Happiness Dashboard'),
+    html.P(['This dashboard shows the happiness score.',
+            html.Br(),
+            html.A('World Happiness Report Data Source',
+                   href='https://worldhappiness.report',
+                   target='_blank')]),
+    dcc.RadioItems(id='region-radio',
+                   options=happiness['region'].unique(),
+                   value='North America'),
+    dcc.Dropdown(id='country-dropdown'), #options and value are not needed as they will now be chained),
+    dcc.RadioItems(id='data-radio',
+                   options={ #must be list or dictionary. keys below are columns in dataset
+                       'happiness_score': 'Happiness Score',
+                       'happiness_rank': 'Hapiness Rank'
+                   },
+                   value='happiness_score'), # initial value
+    dcc.Graph(id='happiness_graph'),
+    html.Div(id='average-div')])
+
+#decorator
+
+#output country dropdown based on selected region
+@app.callback(
+    Output('country-dropdown', 'options'),
+    Output('country-dropdown', 'value'),
+    Input('region-radio', 'value')
+)
+
+def update_dropdown(selected_region):
+    filtered_happiness = happiness[happiness['region'] == selected_region]
+    country_options=filtered_happiness['country'].unique()
+    return country_options, country_options[0] # prev had been pre-selecting 'United States' which no longer makes sense b/c of regional radio filter
+
+@app.callback(
+    Output('happiness_graph', 'figure'),
+    Output('average-div', 'children'),
+    Input('country-dropdown', 'value'),
+    Input('data-radio', 'value')
+)
+
+# order of inputs in the function must match order from the decorator
+def update_graph(selected_country, selected_data):
+    filtered_happiness=happiness[happiness['country']==selected_country]
+    line_fig=px.line(filtered_happiness,
+                     x='year', y=selected_data,
+                     title=f'{selected_data} in {selected_country}')
+    selected_average=filtered_happiness[selected_data].mean()
+    return line_fig, f'The average {selected_data} for {selected_country} is {selected_average}'
+
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
